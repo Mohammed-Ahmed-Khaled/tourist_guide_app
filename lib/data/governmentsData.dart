abe108a0a4da3655369c3b17fdfad9ec3088e936
OmarElDwary <omareldwary91@gmail.com>
2025-01-09 19:37:46 +0200

    adjust localization

--- lib/data/governmentsData.dart
+++ lib/data/governmentsData.dart
@@ -4,7 +4,7 @@ final List governments = [
     "image":
     "https://images.pexels.com/photos/30107330/pexels-photo-30107330.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     "about":
-    "The Sinai peninsula is a peninsula in Egypt, and the only part of the country located in Asia. It is between the Mediterranean Sea to the north and the Red Sea to the south, and is a land bridge between Asia and Africa. Sinai has a land area of about 60,000 km2 (23,000 sq mi) (6 percent of Egypt's total area) and a population of approximately 600,000 people.[1] Administratively, the vast majority of the area of the Sinai Peninsula is divided into two governorates: the South Sinai Governorate and the North Sinai Governorate. Three other governorates span the Suez Canal, crossing into African Egypt: Suez Governorate on the southern end of the Suez Canal, Ismailia Governorate in the center, and Port Said Governorate in the north.",
+    "aboutSinai",
     "landmarks": [
       {
         "landmarkName": "Dahab",
